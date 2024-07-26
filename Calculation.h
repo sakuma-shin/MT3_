c10c61f170804ff932184d370fbe8e101c3f189e
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-07-26 14:01:18 +0900

    03_00を反映

--- Calculation.h
+++ Calculation.h
@@ -136,7 +136,7 @@ void DrawTriangle(const Triangle& triangle, const Matrix4x4& viewProjectionMatri
 
 void DrawAABB(const AABB& aabb, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
 
-void Lerp(const Vector3& v1, const Vector3& v2, float t);
+Vector3 Lerp(const Vector3& v1, const Vector3& v2, float t);
 
 void DrawBezier(const Vector3& controlPoint0, const Vector3& controlPoint1, const Vector3& controlPoint2,
 	const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewPortMatrix, uint32_t color);
