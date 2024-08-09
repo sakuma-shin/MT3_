e4a36e00bd6c4f3541e379ca3387e3e262082125
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-08-09 16:46:12 +0900

    課題完了

--- Calculation.cpp
+++ Calculation.cpp
@@ -669,6 +669,17 @@ void DrawBezier(const Vector3& controlPoint0, const Vector3& controlPoint1, cons
 	}
 }
 
+Vector3 operator+(const Vector3& v1, const Vector3& v2) { return Add(v1, v2); }
+Vector3 operator-(const Vector3& v1, const Vector3& v2) { return Subtract(v1, v2); }
+Vector3 operator*(float s, const Vector3& v) { return Multiply(s, v); }
+Vector3 operator*(const Vector3& v, float s) { return s * v; }
+Vector3 operator/(const Vector3& v, float s) { return Multiply(1.0f / s, v); }
+Matrix4x4 operator+(const Matrix4x4& m1, const Matrix4x4& m2) { return Add(m1, m2); }
+Matrix4x4 operator-(const Matrix4x4& m1, const Matrix4x4& m2) { return Subtract(m1, m2); }
+Matrix4x4 operator*(const Matrix4x4& m1, const Matrix4x4& m2) { return Multiply(m1, m2); }
+
+Vector3 operator-(const Vector3& v) { return{ -v.x,-v.y,-v.z }; }
+Vector3 operator+(const Vector3& v) { return v; }
 
 
 
